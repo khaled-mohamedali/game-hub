a0e13f532b6862ae9c072af4e88c388c98a8d7b2
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-04-01 16:50:32 -0400

    Built Platform Selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
+import { useState } from "react";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
-import { useState } from "react";
+import NavBar from "./components/NavBar";
+import PlatformSelector from "./components/PlatformSelector";
 import { Genre } from "./hooks/useGenres";
 
 function App() {
   const isLg = useBreakpointValue({ base: false, lg: true });
   const [seletedGenre, setSelectedGenre] = useState<Genre | null>(null);
+
   return (
     <Grid
       templateAreas={{
@@ -27,6 +29,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area={"main"}>
+        <PlatformSelector></PlatformSelector>
         <GameGrid seletedGenre={seletedGenre}></GameGrid>
       </GridItem>
     </Grid>
---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,28 @@
+import usePlatforms from "@/hooks/usePlatform";
+import { Button, Menu, Portal } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+  if (error) return null;
+  return (
+    <Menu.Root>
+      <Menu.Trigger asChild>
+        <Button variant="outline" size="sm">
+          <BsChevronDown /> Platforms
+        </Button>
+      </Menu.Trigger>
+      <Portal>
+        <Menu.Positioner>
+          <Menu.Content>
+            {data.map((platform) => (
+              <Menu.Item value={platform.slug}>{platform.slug}</Menu.Item>
+            ))}
+          </Menu.Content>
+        </Menu.Positioner>
+      </Portal>
+    </Menu.Root>
+  );
+};
+
+export default PlatformSelector;

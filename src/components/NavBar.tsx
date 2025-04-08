bb33aab52f260492b3d5ae2f3b1b98436f2ce6d3
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-04-08 18:53:13 -0400

    Create a new Logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,13 @@
 import { ColorModeButton } from "@/components/ui/color-mode";
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/Logo/logo.webp";
+import logo from "../assets/Logo/game-hub.png";
 import SearchInput from "./SearchInput";
 import { SearchProps } from "./SearchInput";
 
 const NavBar = ({ onSearch }: SearchProps) => {
   return (
     <HStack justifyContent={"space-between"} padding={5}>
-      <Image src={logo} boxSize={55}></Image>
+      <Image src={logo} boxSize={55} borderRadius={10}></Image>
       <SearchInput onSearch={onSearch}></SearchInput>
       <ColorModeButton scale={1.5}></ColorModeButton>
     </HStack>
